1550cf955085892bc72013c614e0dfcad1df4c1c
pozdeyev <pozdeyev@gmail.com>
2019-04-09 20:02:48 +0300

    Vector

--- core/src/ru/geekbrains/CosmicShooter.java
+++ core/src/ru/geekbrains/CosmicShooter.java
@@ -6,40 +6,57 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class CosmicShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
-	//TextureRegion region;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("nebulabackground.jpg");
-		//region = new TextureRegion(img, 50,50,150,150);
-		//region.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-	}
+    SpriteBatch batch;
+    Texture background;
 
-	@Override
-	public void render () {
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("nebulabackground.jpg");
 
-		int width = Gdx.app.getGraphics().getWidth();
-		int height = Gdx.app.getGraphics().getHeight();
+        Vector2 v1=new Vector2();
+        Vector2 v2=new Vector2();
+        Vector2 v3;
+        v1.set(5,5);
+        v2.set(2,1);
+        v1.add(v2);
+        System.out.println("add v.x = "+v1.x+" v.y = "+v1.y);
+        v1.set(10,6);
+        v2.set(3,11);
+        v1.sub(v2);
+        v3=v1.cpy().sub(v2);
+        System.out.println("sub v.x = "+v1.x+" v.y = "+v1.y);
+        System.out.println("sub v.x = "+v3.x+" v.y = "+v3.y);
+        System.out.println("v1.len = " + v1.len());
+        System.out.println("v3.len = " + v3.len());
+        v3.nor();
+        System.out.println("sub v.x = "+v3.x+" v.y = "+v3.y);
+        System.out.println("v3.len = " + v3.len());
 
-		//Gdx.gl.glClearColor(0.25f,0.15f, 0.37f,  1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+        v1.set(4,5);
+        v2.set(5,13);
 
-		batch.draw(background,0,0,width, height);
+        v1.nor();
+        v2.nor();
 
-		//batch.setColor(0.666f,0.777f,0.13f,1f);
-		//batch.draw(region,100,50,100,100);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		background.dispose();
-	}
+        System.out.println(Math.acos((v1.dot(v2))));
+    }
+
+    @Override
+    public void render() {
+        int width = Gdx.app.getGraphics().getWidth();
+        int height = Gdx.app.getGraphics().getHeight();
+        batch.begin();
+        batch.draw(background, 0, 0, width, height);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        background.dispose();
+    }
 }
