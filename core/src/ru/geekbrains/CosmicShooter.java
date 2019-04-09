a57961bce6338d481afea2d8711d49f45e1640b8
pozdeyev <pozdeyev@gmail.com>
2019-04-10 00:20:20 +0300

    Homework2

--- core/src/ru/geekbrains/CosmicShooter.java
+++ core/src/ru/geekbrains/CosmicShooter.java
@@ -1,62 +1,17 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 
-public class CosmicShooter extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture background;
+import com.badlogic.gdx.Game;
+
+
+import ru.geekbrains.screen.MenuScreen;
+
+public class CosmicShooter extends Game {
+
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        background = new Texture("nebulabackground.jpg");
-
-        Vector2 v1=new Vector2();
-        Vector2 v2=new Vector2();
-        Vector2 v3;
-        v1.set(5,5);
-        v2.set(2,1);
-        v1.add(v2);
-        System.out.println("add v.x = "+v1.x+" v.y = "+v1.y);
-        v1.set(10,6);
-        v2.set(3,11);
-        v1.sub(v2);
-        v3=v1.cpy().sub(v2);
-        System.out.println("sub v.x = "+v1.x+" v.y = "+v1.y);
-        System.out.println("sub v.x = "+v3.x+" v.y = "+v3.y);
-        System.out.println("v1.len = " + v1.len());
-        System.out.println("v3.len = " + v3.len());
-        v3.nor();
-        System.out.println("sub v.x = "+v3.x+" v.y = "+v3.y);
-        System.out.println("v3.len = " + v3.len());
-
-        v1.set(4,5);
-        v2.set(5,13);
-
-        v1.nor();
-        v2.nor();
-
-        System.out.println(Math.acos((v1.dot(v2))));
+        setScreen(new MenuScreen());
     }
 
-    @Override
-    public void render() {
-        int width = Gdx.app.getGraphics().getWidth();
-        int height = Gdx.app.getGraphics().getHeight();
-        batch.begin();
-        batch.draw(background, 0, 0, width, height);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        background.dispose();
-    }
 }
---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,66 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private static final float SPEEDCONST = 2;
+    private static final float SIZEFACTOR = 0.5f;
+    private static final float TOLERANCE = 1;
+    private Vector2 touch;
+    private Vector2 position;
+    private Vector2 speed;
+    private Texture img;
+    private float sizeX;
+    private float sizeY;
+
+    @Override
+    public void show() {
+        super.show();
+        touch = new Vector2();    //позиция мыши
+        position = new Vector2(5, 5); //позиция
+        speed = new Vector2(); //вектор скорости
+        img = new Texture("badlogic.jpg");
+        sizeX = img.getWidth() * SIZEFACTOR;
+        sizeY = img.getHeight() * SIZEFACTOR;
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        position.add(speed);
+        batch.begin();
+        batch.draw(img, position.x, position.y, sizeX, sizeY);
+        batch.end();
+
+        //Остановка по достижении границ экрана
+        if ((position.y >= Gdx.graphics.getHeight() - sizeY) || (position.y <= 0) ||
+                (position.x >= Gdx.graphics.getWidth() - sizeX) || (position.x <= 0)) {
+            speed.setZero();
+        }
+        //Остановка по достижении координат
+        if ((Math.abs(touch.y - position.y) < TOLERANCE) &&
+                (Math.abs(touch.x - position.x) <TOLERANCE)) speed.setZero();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        System.out.println("touch.x = " + touch.x + " touch.y = " + touch.y);
+        speed = touch.cpy().sub(position); //определяем вектор скорости
+        speed.setLength(SPEEDCONST); //задаем длину вектора скорости до заданного значения
+        return false;
+    }
+}
